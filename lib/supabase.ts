a678401fc17b507de9d24e5561f2216310f241f8
vivatecpe-dot <vivatecpe@gmail.com>
2026-01-23 15:40:22 -0500

    Add files via upload

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,1 +1,7 @@
 
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://zjuemuaqrnijrnjvxpfk.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqdWVtdWFxcm5panJuanZ4cGZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxODQ0OTYsImV4cCI6MjA4NDc2MDQ5Nn0.COdcvvzBABH0PHFooIy_UyVWVD56loJA6qMVt6_eOdQ';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
